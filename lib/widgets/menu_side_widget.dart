3b8d4a5dc1b8e4ffed7f275fcfdc6877b94ac42d
lucland <lucas.vsc@gmail.com>
2024-02-01 17:03:29 -0300

    Add MenuSide widget

---
+++ lib/widgets/menu_side_widget.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../utils/colors.dart';
+import '../utils/theme.dart';
+
+class MenuSide extends StatelessWidget {
+  const MenuSide({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: MediaQuery.of(context).size.height,
+      color: DockColors.iron100,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SvgPicture.asset(
+                  'assets/svg/logo_web.svg',
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.anchor,
+                        color: DockColors.white,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Projetos',
+                        overflow: TextOverflow.ellipsis,
+                        style: DockTheme.h2.copyWith(
+                            color: DockColors.background,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.person_outline_outlined,
+                        color: DockColors.white,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Funcionários',
+                        overflow: TextOverflow.ellipsis,
+                        style: DockTheme.h2.copyWith(
+                            color: DockColors.background,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 40,
+                decoration: BoxDecoration(
+                  color: DockColors.iron100,
+                  borderRadius: BorderRadius.circular(2),
+                  border: Border.all(
+                    color: DockColors.white,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.logout,
+                        color: DockColors.white,
+                        size: 16,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Logout',
+                        overflow: TextOverflow.ellipsis,
+                        style: DockTheme.h2
+                            .copyWith(color: DockColors.white, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
