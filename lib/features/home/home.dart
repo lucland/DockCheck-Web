7a02f7e58a54d64048200d371bde422580c2ad38
lucland <lucas.vsc@gmail.com>
2024-02-07 18:09:18 -0300

    Fix bug in login functionality

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,8 +1,13 @@
+import 'package:dockcheck_web/features/projects/projects.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../utils/colors.dart';
+import '../../utils/theme.dart';
 import '../../widgets/funcionarios_widget.dart';
-import '../../widgets/menu_side_widget.dart';
+//import flutter svg
+import 'package:flutter_svg/flutter_svg.dart';
+import '../login/login.dart';
 import 'bloc/pesquisar_cubit.dart';
 
 class Home extends StatefulWidget {
@@ -24,14 +29,267 @@ class _HomeState extends State<Home> {
     )
   ];
 
+  String selectedPage = 'projetos';
+
   @override
   Widget build(BuildContext context) {
     context.read<PesquisarCubit>().fetchUsers();
     return Scaffold(
       body: Row(
         children: [
-          const MenuSide(),
-          FuncionariosWidget(listWidget: listWidget),
+          Container(
+            width: 300,
+            height: MediaQuery.of(context).size.height,
+            color: DockColors.iron100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SvgPicture.asset(
+                        'assets/svg/logo_web.svg',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: DockColors.iron30,
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'DOF',
+                            style: DockTheme.h1.copyWith(
+                              color: DockColors.iron100,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectedPage = 'projetos';
+                        });
+                      },
+                      child: Container(
+                        color: selectedPage == 'projetos'
+                            ? DockColors.background
+                            : DockColors.iron100,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 4.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.anchor,
+                                color: selectedPage == 'projetos'
+                                    ? DockColors.iron100
+                                    : DockColors.background,
+                                size: 20,
+                              ),
+                              const SizedBox(
+                                width: 16,
+                              ),
+                              Text(
+                                'Projetos',
+                                overflow: TextOverflow.ellipsis,
+                                style: DockTheme.h2.copyWith(
+                                    color: selectedPage == 'projetos'
+                                        ? DockColors.iron100
+                                        : DockColors.background,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectedPage = 'funcionarios';
+                        });
+                      },
+                      child: Container(
+                        color: selectedPage == 'funcionarios'
+                            ? DockColors.background
+                            : DockColors.iron100,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 4.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.person_outline_outlined,
+                                color: selectedPage == 'funcionarios'
+                                    ? DockColors.iron100
+                                    : DockColors.background,
+                                size: 20,
+                              ),
+                              const SizedBox(
+                                width: 16,
+                              ),
+                              Text(
+                                'Funcionários',
+                                overflow: TextOverflow.ellipsis,
+                                style: DockTheme.h2.copyWith(
+                                    color: selectedPage == 'funcionarios'
+                                        ? DockColors.iron100
+                                        : DockColors.background,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectedPage = 'dashboard';
+                        });
+                      },
+                      child: Container(
+                        color: selectedPage == 'dashboard'
+                            ? DockColors.background
+                            : DockColors.iron100,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 4.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.dashboard_outlined,
+                                color: selectedPage == 'dashboard'
+                                    ? DockColors.iron100
+                                    : DockColors.background,
+                                size: 20,
+                              ),
+                              const SizedBox(
+                                width: 16,
+                              ),
+                              Text(
+                                'Dashboard',
+                                overflow: TextOverflow.ellipsis,
+                                style: DockTheme.h2.copyWith(
+                                    color: selectedPage == 'dashboard'
+                                        ? DockColors.iron100
+                                        : DockColors.background,
+                                    fontSize: 20,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                //text with version
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Divider(
+                        color: DockColors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Versão 1.0.0',
+                          style: DockTheme.h2.copyWith(
+                            color: DockColors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.9, 16.0),
+                        child: Text(
+                          '© 2024 DockCheck. Todos os direitos reservados.',
+                          style: DockTheme.h2.copyWith(
+                            color: DockColors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: DockColors.iron100,
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(
+                              color: DockColors.white,
+                              width: 2,
+                            ),
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.logout,
+                                  color: DockColors.white,
+                                  size: 16,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Sair',
+                                  overflow: TextOverflow.ellipsis,
+                                  style: DockTheme.h2.copyWith(
+                                      color: DockColors.white, fontSize: 16),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (selectedPage == 'funcionarios') ...{
+            FuncionariosWidget(listWidget: listWidget),
+          } else if (selectedPage == 'projetos') ...{
+            const Projects(),
+          } else if (selectedPage == 'dashboard') ...{
+            Container(
+              color: Colors.green,
+              height: 500,
+            )
+          }
         ],
       ),
     );
