98b81c3b43d028af405f05cda16c0e925078bb6d
lucland <lucas.vsc@gmail.com>
2024-02-27 14:40:21 -0300

    Update CadastrarState with Employee model, Document model, and Picture model

--- lib/features/home/bloc/cadastrar_state.dart
+++ lib/features/home/bloc/cadastrar_state.dart
@@ -1,49 +1,60 @@
+import 'package:dockcheck_web/models/document.dart';
+
 import '../../../models/event.dart';
-import '../../../models/user.dart';
+import '../../../models/employee.dart';
+import '../../../models/picture.dart';
 
 class CadastrarState {
   final int numero;
-  final User user;
+  final Employee employee;
   final bool isLoading;
   final String? errorMessage;
-  final Event evento;
-  final bool userCreated;
+  final Event event;
+  final bool employeeCreated;
   final bool cadastroHabilitado;
   final List<String> nrTypes;
+  final List<Document> documents;
+  final Picture picture;
   final String selectedNr;
 
   CadastrarState({
     this.numero = 0,
-    required this.user,
+    required this.employee,
     this.isLoading = true,
     this.errorMessage,
-    required this.evento,
-    this.userCreated = false,
+    required this.event,
+    this.employeeCreated = false,
     this.cadastroHabilitado = false,
     this.nrTypes = const [],
+    this.documents = const [],
+    required this.picture,
     this.selectedNr = '',
   });
 
   CadastrarState copyWith({
     int? numero,
-    User? user,
+    Employee? employee,
     bool? isLoading,
     String? errorMessage,
-    Event? evento,
-    bool? userCreated,
+    Event? event,
+    bool? employeeCreated,
     bool? cadastroHabilitado,
     List<String>? nrTypes,
+    List<Document>? documents,
+    Picture? picture,
     String? selectedNr,
   }) {
     return CadastrarState(
       numero: numero ?? this.numero,
-      user: user ?? this.user,
+      employee: employee ?? this.employee,
       isLoading: isLoading ?? this.isLoading,
       errorMessage: errorMessage,
-      evento: evento ?? this.evento,
-      userCreated: userCreated ?? this.userCreated,
+      event: event ?? this.event,
+      employeeCreated: employeeCreated ?? this.employeeCreated,
       cadastroHabilitado: cadastroHabilitado ?? this.cadastroHabilitado,
       nrTypes: nrTypes ?? this.nrTypes,
+      documents: documents ?? this.documents,
+      picture: picture ?? this.picture,
       selectedNr: selectedNr ?? this.selectedNr,
     );
   }
