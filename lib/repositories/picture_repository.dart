ee9436d42e6ee08803de3cfad209baee71955f34
lucland <lucas.vsc@gmail.com>
2024-02-27 16:27:00 -0300

    Refactor picture repository methods

--- lib/repositories/picture_repository.dart
+++ lib/repositories/picture_repository.dart
@@ -1,27 +1,51 @@
-import '../models/picture.dart';
+import 'dart:async';
+
+import '../models/picture.dart'; // Make sure to import the corresponding model
 import '../services/api_service.dart';
+import '../utils/simple_logger.dart';
 
 class PictureRepository {
   final ApiService apiService;
 
   PictureRepository(this.apiService);
 
-  Future<Picture> createPicture(Picture picture) async {
-    final data = await apiService.post('api/v1/pictures', picture.toJson());
-    return Picture.fromJson(data);
+  Future<Picture> createEmployeePicture(Picture picture) async {
+    try {
+      final data = await apiService.post('pictures', picture.toJson());
+      return Picture.fromJson(data);
+    } catch (e) {
+      SimpleLogger.severe('Failed to create picture: ${e.toString()}');
+      return picture;
+    }
   }
 
-  Future<Picture> getPicture(String id) async {
-    final data = await apiService.get('api/v1/pictures/$id');
-    return Picture.fromJson(data);
+  Future<Picture?> getPictureByEmployeeId(String employeeId) async {
+    try {
+      final data = await apiService.get('pictures/employee/$employeeId');
+      return Picture.fromJson(data);
+    } catch (e) {
+      SimpleLogger.severe('Failed to get picture: ${e.toString()}');
+      return null; // Return null as a fallback
+    }
   }
 
   Future<Picture> updatePicture(String id, Picture picture) async {
-    final data = await apiService.put('api/v1/pictures/$id', picture.toJson());
-    return Picture.fromJson(data);
+    try {
+      final data = await apiService.put('pictures/$id', {'pic': picture});
+      return Picture.fromJson(data);
+    } catch (e) {
+      SimpleLogger.severe('Failed to update picture: ${e.toString()}');
+      return picture;
+    }
   }
 
-  Future<void> deletePicture(String id) async {
-    await apiService.delete('api/v1/pictures/$id');
+  Future<bool> deletePicture(String id) async {
+    try {
+      final data = await apiService.delete('pictures/$id');
+      return data['message'] == 'Picture deleted successfully';
+    } catch (e) {
+      SimpleLogger.severe('Failed to delete picture: ${e.toString()}');
+      return false;
+    }
   }
 }
