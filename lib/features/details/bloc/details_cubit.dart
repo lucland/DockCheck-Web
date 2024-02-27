061aee86b5bd7f5c9c4053113ee66a2507b06c62
lucland <lucas.vsc@gmail.com>
2024-02-27 16:23:52 -0300

    Add employee and document repositories to DetailsCubit

--- lib/features/details/bloc/details_cubit.dart
+++ lib/features/details/bloc/details_cubit.dart
@@ -1,27 +1,39 @@
+import 'package:dockcheck_web/models/employee.dart';
+import 'package:dockcheck_web/repositories/document_repository.dart';
+import 'package:dockcheck_web/repositories/employee_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../models/user.dart';
-import '../../../repositories/user_repository.dart';
+import '../../../models/document.dart';
 import '../../../services/local_storage_service.dart';
 import '../../../utils/simple_logger.dart';
 
 part 'details_state.dart';
 
 class DetailsCubit extends Cubit<DetailsState> {
-  final UserRepository userRepository;
+  final EmployeeRepository employeeRepository;
+  final DocumentRepository documentsRepository; // Added DocumentsRepository
   final LocalStorageService localStorageService;
 
-  DetailsCubit(this.userRepository, this.localStorageService)
+  DetailsCubit(this.employeeRepository, this.documentsRepository,
+      this.localStorageService)
       : super(DetailsInitial());
 
-  Future<void> getUser(String userId) async {
+  Future<void> getEmployeeAndDocuments(String employeeId) async {
     try {
       emit(DetailsLoading());
-      final user = await userRepository.getUser(userId);
-      emit(DetailsLoaded(user));
+      final employee = await employeeRepository.getEmployeeById(employeeId);
+      final documentIds =
+          await documentsRepository.getDocumentIdsByEmployeeId(employeeId);
+      final documents = await Future.wait<Document>(
+          documentIds.map((id) => documentsRepository.getDocumentById(id)));
+      List<Document> filteredDocuments = documents
+          .where((document) => document.type.isNotEmpty)
+          .toList()
+          .cast<Document>();
+      emit(DetailsLoaded(employee, filteredDocuments));
     } catch (e) {
-      SimpleLogger.warning('Error fetching user: $e');
-      emit(DetailsError('Failed to fetch user: $e'));
+      SimpleLogger.warning('Error fetching user or documents: $e');
+      emit(DetailsError('Failed to fetch user or documents: $e'));
     }
   }
 }
