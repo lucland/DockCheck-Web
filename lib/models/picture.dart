d47454c01cb3eab776de023b2c71ac05432cb2f0
lucland <lucas.vsc@gmail.com>
2024-02-27 16:25:27 -0300

    Refactor Picture model to use employeeId, base64, docPath, and status

--- lib/models/picture.dart
+++ lib/models/picture.dart
@@ -1,27 +1,51 @@
 class Picture {
   String id;
-  String userId;
-  String picture;
+  String employeeId;
+  String base64;
+  String docPath;
+  String status;
 
   Picture({
     required this.id,
-    required this.userId,
-    required this.picture,
+    required this.employeeId,
+    required this.base64,
+    required this.docPath,
+    required this.status,
   });
 
   factory Picture.fromJson(Map<String, dynamic> json) {
     return Picture(
       id: json['id'],
-      userId: json['user_id'],
-      picture: json['picture'],
+      employeeId: json['employee_id'],
+      base64: json['base_64'],
+      docPath: json['doc_path'],
+      status: json['status'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'user_id': userId,
-      'picture': picture,
+      'employee_id': employeeId,
+      'base_64': base64,
+      'doc_path': docPath,
+      'status': status,
     };
+  }
+
+  Picture copyWith({
+    String? id,
+    String? employeeId,
+    String? base64,
+    String? docPath,
+    String? status,
+  }) {
+    return Picture(
+      id: id ?? this.id,
+      employeeId: employeeId ?? this.employeeId,
+      base64: base64 ?? this.base64,
+      docPath: docPath ?? this.docPath,
+      status: status ?? this.status,
+    );
   }
 }
