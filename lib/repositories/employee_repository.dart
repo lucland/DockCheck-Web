b43f0c6463c4c842dc62d375e7e5e84211b439ad
lucland <lucas.vsc@gmail.com>
2024-02-27 16:26:47 -0300

    Add EmployeeRepository class with CRUD operations

---
+++ lib/repositories/employee_repository.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import '../models/employee.dart'; // Make sure to import the corresponding model
+import '../services/api_service.dart';
+import '../utils/simple_logger.dart';
+
+class EmployeeRepository {
+  final ApiService apiService;
+
+  EmployeeRepository(this.apiService);
+
+  Future<Employee> createEmployee(Employee employee) async {
+    try {
+      final data = await apiService.post('employees', employee.toJson());
+      return Employee.fromJson(data);
+    } catch (e) {
+      SimpleLogger.severe('Failed to create employee: ${e.toString()}');
+      return employee;
+    }
+  }
+
+  Future<void> blockEmployee(String id, String blockReason) async {
+    try {
+      await apiService
+          .post('employees/$id/block', {'block_reason': blockReason});
+      SimpleLogger.info('Employee blocked successfully');
+    } catch (e) {
+      SimpleLogger.severe('Failed to block employee: ${e.toString()}');
+    }
+  }
+
+  Future<Employee> getEmployeeById(String id) async {
+    final data = await apiService.get('employees/$id');
+    return Employee.fromJson(data);
+  }
+
+  Future<List<Employee>> getAllEmployees() async {
+    try {
+      final data = await apiService.get('employees');
+      return (data as List).map((item) => Employee.fromJson(item)).toList();
+    } catch (e) {
+      SimpleLogger.severe('Failed to get employees: ${e.toString()}');
+      return []; // Return an empty list as a fallback
+    }
+  }
+
+  //getLastEmployeeNumber
+  Future<String> getLastEmployeeNumber() async {
+    try {
+      final data = await apiService.get('employees/lastnumber');
+      return data['number'];
+    } catch (e) {
+      SimpleLogger.severe(
+          'Failed to get last employee number: ${e.toString()}');
+      return '0'; // Return 0 as a fallback
+    }
+  }
+
+  Future<Employee> updateEmployee(String id, Employee employee) async {
+    try {
+      final data = await apiService.put('employees/$id', employee.toJson());
+      return Employee.fromJson(data);
+    } catch (e) {
+      SimpleLogger.severe('Failed to update employee: ${e.toString()}');
+      return employee;
+    }
+  }
+
+  Future<void> updateEmployeeArea(
+      String id, String lastAreaFound, DateTime lastTimeFound) async {
+    try {
+      await apiService.put('employees/$id/area', {
+        'last_area_found': lastAreaFound,
+        'last_time_found': lastTimeFound.toString()
+      });
+      SimpleLogger.info('Employee area updated successfully');
+    } catch (e) {
+      SimpleLogger.severe('Failed to update employee area: ${e.toString()}');
+    }
+  }
+}
