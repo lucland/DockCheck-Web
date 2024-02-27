242e73e19a11858cebb66bcf45475505750e0f06
lucland <lucas.vsc@gmail.com>
2024-02-27 16:23:18 -0300

    Update imports and repositories in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dockcheck_web/features/home/bloc/pesquisar_cubit.dart';
 import 'package:dockcheck_web/features/home/home.dart';
-import 'package:dockcheck_web/features/login/login.dart';
 import 'package:dockcheck_web/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,15 +8,18 @@ import 'package:provider/provider.dart';
 import 'features/details/bloc/details_cubit.dart';
 import 'features/home/bloc/cadastrar_cubit.dart';
 import 'features/login/bloc/login_cubit.dart';
+import 'repositories/area_repository.dart';
 import 'repositories/authorization_repository.dart';
 import 'repositories/beacon_repository.dart';
 import 'repositories/company_repository.dart';
-import 'repositories/docking_repository.dart';
+import 'repositories/document_repository.dart';
+import 'repositories/employee_repository.dart';
 import 'repositories/event_repository.dart';
+import 'repositories/invite_repository.dart';
 import 'repositories/login_repository.dart';
-import 'repositories/portal_repository.dart';
-import 'repositories/receptor_repository.dart';
-import 'repositories/supervisor_repository.dart';
+import 'repositories/picture_repository.dart';
+import 'repositories/third_company_repository.dart';
+import 'repositories/third_project_repository.dart';
 import 'repositories/user_repository.dart';
 import 'repositories/vessel_repository.dart';
 import 'services/api_service.dart';
@@ -29,24 +31,27 @@ void main() {
   var apiService = ApiService(localStorageService);
 
   var loginRepository = LoginRepository(apiService);
-  var authorizationRepository =
-      AuthorizationRepository(apiService, localStorageService);
-  var companyRepository = CompanyRepository(apiService, localStorageService);
-  var dockingRepository = DockingRepository(apiService, localStorageService);
+  var authorizationRepository = AuthorizationRepository(apiService);
+  var companyRepository = CompanyRepository(apiService);
   var eventRepository = EventRepository(apiService, localStorageService);
-  var portalRepository = PortalRepository(apiService, localStorageService);
-  var supervisorRepository =
-      SupervisorRepository(apiService, localStorageService);
   var userRepository = UserRepository(apiService, localStorageService);
-  var vesselRepository = VesselRepository(apiService, localStorageService);
-  var beaconRepository = BeaconRepository(apiService, localStorageService);
-  var receptorRepository = ReceptorRepository(apiService, localStorageService);
+  var vesselRepository = VesselRepository(apiService);
+  var beaconRepository = BeaconRepository(apiService);
+  var areaRepository = AreaRepository(apiService);
+  var documentRepository = DocumentRepository(apiService);
+  var employeeRepository = EmployeeRepository(apiService);
+  var inviteRepository = InviteRepository(apiService);
+  var pictureRepository = PictureRepository(apiService);
+  var thirdCompanyRepository = ThirdCompanyRepository(apiService);
+  var thirdProjectRepository = ThirdProjectRepository(apiService);
 
   var loginCubit =
       LoginCubit(loginRepository, userRepository, localStorageService);
-  var pesquisarCUbit = PesquisarCubit(userRepository);
-  var cadastrarCubit = CadastrarCubit(userRepository, localStorageService);
-  var detailsCubit = DetailsCubit(userRepository, localStorageService);
+  var pesquisarCUbit = PesquisarCubit(employeeRepository);
+  var cadastrarCubit = CadastrarCubit(employeeRepository, localStorageService,
+      eventRepository, pictureRepository, documentRepository);
+  var detailsCubit =
+      DetailsCubit(employeeRepository, documentRepository, localStorageService);
 
   runApp(
     MultiBlocProvider(
@@ -55,17 +60,20 @@ void main() {
         Provider<AuthorizationRepository>(
             create: (_) => authorizationRepository),
         Provider<CompanyRepository>(create: (_) => companyRepository),
-        Provider<DockingRepository>(create: (_) => dockingRepository),
         Provider<EventRepository>(create: (_) => eventRepository),
-        Provider<PortalRepository>(create: (_) => portalRepository),
-        Provider<SupervisorRepository>(create: (_) => supervisorRepository),
         Provider<UserRepository>(create: (_) => userRepository),
         Provider<VesselRepository>(create: (_) => vesselRepository),
         Provider<ApiService>(create: (_) => apiService),
         Provider<LocalStorageService>(create: (_) => localStorageService),
         BlocProvider<LoginCubit>(create: (_) => loginCubit),
         Provider<BeaconRepository>(create: (_) => beaconRepository),
-        Provider<ReceptorRepository>(create: (_) => receptorRepository),
+        Provider<AreaRepository>(create: (_) => areaRepository),
+        Provider<DocumentRepository>(create: (_) => documentRepository),
+        Provider<EmployeeRepository>(create: (_) => employeeRepository),
+        Provider<InviteRepository>(create: (_) => inviteRepository),
+        Provider<PictureRepository>(create: (_) => pictureRepository),
+        Provider<ThirdCompanyRepository>(create: (_) => thirdCompanyRepository),
+        Provider<ThirdProjectRepository>(create: (_) => thirdProjectRepository),
         BlocProvider<PesquisarCubit>(create: (_) => pesquisarCUbit),
         BlocProvider<CadastrarCubit>(create: (_) => cadastrarCubit),
         BlocProvider<DetailsCubit>(create: (_) => detailsCubit),
