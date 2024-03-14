9294a803ab5a29d2f92f984cdd4af90dd75dead5
lucland <lucas.vsc@gmail.com>
2024-03-14 00:35:39 -0300

    Fix bug in login functionality

--- lib/widgets/project_modal_widget.dart
+++ lib/widgets/project_modal_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:dockcheck_web/widgets/file_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart'; // Import Intl package for date formatting
@@ -14,6 +16,13 @@ class ProjectModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String name = '';
+    String vesselId = '';
+    DateTime startDate = DateTime.now();
+    DateTime endDate = DateTime.now();
+    bool isDocking = true;
+    String address = '';
+
     return BlocBuilder<ProjectCubit, ProjectState>(
       builder: (context, state) {
         return AlertDialog(
@@ -32,18 +41,36 @@ class ProjectModal extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   TextInputWidget(
-                    title: 'Nome do Projeto',
-                    isRequired: true,
-                    controller: TextEditingController(),
-                    onChanged: (text) =>
-                        context.read<ProjectCubit>().updateName(text),
-                  ),
+                      title: 'Nome do Projeto',
+                      isRequired: true,
+                      controller: TextEditingController(
+                        text: name,
+                      ),
+                      onChanged: (text) {
+                        name = text;
+                      }),
+                  SizedBox(height: 8),
+                  TextInputWidget(
+                      title: 'Nome da Embarcação',
+                      isRequired: true,
+                      controller: TextEditingController(
+                        text: vesselId,
+                      ),
+                      onChanged: (text) {
+                        vesselId = text;
+                      }),
                   SizedBox(height: 8),
                   CalendarPickerWidget(
                     title: 'Data de Início',
                     isRequired: true,
                     showAttachmentIcon: false,
-                    controller: TextEditingController(),
+                    controller: TextEditingController(
+                      text: state.startDate!.day.toString() +
+                          '/' +
+                          state.startDate!.month.toString() +
+                          '/' +
+                          state.startDate!.year.toString(),
+                    ),
                     onChanged: (date) =>
                         context.read<ProjectCubit>().updateStartDate(date),
                   ),
@@ -52,27 +79,32 @@ class ProjectModal extends StatelessWidget {
                     title: 'Data de Término',
                     isRequired: true,
                     showAttachmentIcon: false,
-                    controller: TextEditingController(),
+                    controller: TextEditingController(
+                      text: state.endDate!.day.toString() +
+                          '/' +
+                          state.endDate!.month.toString() +
+                          '/' +
+                          state.endDate!.year.toString(),
+                    ),
                     onChanged: (date) =>
                         context.read<ProjectCubit>().updateEndDate(date),
                   ),
                   SizedBox(height: 8),
                   Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         'Tipo de Projeto',
                         style: DockTheme.h2,
-                        overflow: TextOverflow.ellipsis,
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Row(
                           children: [
                             Container(
-                             
                               child: ChoiceChip(
-                                label: Text(state.isDocking ? 'Docagem' : 'Docagem'),
+                                label: Text(
+                                    state.isDocking ? 'Docagem' : 'Docagem'),
                                 selected: state.isDocking,
                                 onSelected: (bool selected) {
                                   // Toggle the state.isDocking value
@@ -80,8 +112,8 @@ class ProjectModal extends StatelessWidget {
                                       .read<ProjectCubit>()
                                       .updateIsDocking(!state.isDocking);
                                 }, // Adjust the color as needed
-                                selectedColor:
-                                    DockColors.iron100, // Adjust the color as needed
+                                selectedColor: DockColors
+                                    .iron100, // Adjust the color as needed
                                 labelStyle: TextStyle(
                                   color: Colors.white,
                                 ),
@@ -89,11 +121,11 @@ class ProjectModal extends StatelessWidget {
                               ),
                             ),
                             Container(
-                              padding: const EdgeInsets.fromLTRB(
-                              8,0,0,0),
+                              padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
                               child: ChoiceChip(
-                                label: Text(
-                                    state.isDocking ? 'Mobilização' : 'Mobilização'),
+                                label: Text(state.isDocking
+                                    ? 'Mobilização'
+                                    : 'Mobilização'),
                                 selected: !state.isDocking,
                                 onSelected: (bool selected) {
                                   // Toggle the state.isDocking value
@@ -101,15 +133,14 @@ class ProjectModal extends StatelessWidget {
                                       .read<ProjectCubit>()
                                       .updateIsDocking(!state.isDocking);
                                 }, // Adjust the color as needed
-                                selectedColor:
-                                    DockColors.iron100, // Adjust the color as needed
+                                selectedColor: DockColors
+                                    .iron100, // Adjust the color as needed
                                 labelStyle: TextStyle(
                                   color: Colors.white,
                                 ),
                                 backgroundColor: Colors.grey,
                               ),
                             ),
-                             
                           ],
                         ),
                       ),
@@ -118,21 +149,20 @@ class ProjectModal extends StatelessWidget {
                   SizedBox(height: 8),
                   // New TextInputWidget for "Endereço"
                   TextInputWidget(
-                    title: 'Endereço',
-                    isRequired: false, // Set based on your requirement
-                    controller: TextEditingController(),
-                    onChanged: (text) => context
-                        .read<ProjectCubit>()
-                        .updateName(
-                            text), // Implement this method in your cubit
-                  ),
+                      title: 'Endereço',
+                      isRequired: false, // Set based on your requirement
+                      controller: TextEditingController(
+                        text: state.address,
+                      ),
+                      onChanged: (text) {
+                        address = text;
+                      } // Implement this method in your cubit
+                      ),
                   SizedBox(height: 8),
                   // New FilePickerWidget for "Blueprints do projeto"
                   FilePickerWidget(
                     title: 'Blueprints do Projeto',
-                    
                   ),
-                  
                 ],
               ),
             ),
@@ -145,11 +175,13 @@ class ProjectModal extends StatelessWidget {
             ),
             TextButton(
               onPressed: () {
-                context.read<ProjectCubit>().createProject();
+                context
+                    .read<ProjectCubit>()
+                    .createProject(name, vesselId, address);
                 Navigator.of(context).pop();
               },
               child: Text('Criar Projeto',
-                  style: TextStyle(color: DockColors.success120)),
+                  style: TextStyle(color: DockColors.iron100)),
             ),
           ],
         );
