16c73cc7f89877256f818ec4c049501962221a72
lucland <lucas.vsc@gmail.com>
2024-03-27 16:06:05 -0300

    Add Firebase Storage integration and fetch document URLs

--- lib/features/details/bloc/details_cubit.dart
+++ lib/features/details/bloc/details_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:dockcheck_web/models/employee.dart';
+import 'package:dockcheck_web/models/picture.dart';
 import 'package:dockcheck_web/repositories/document_repository.dart';
 import 'package:dockcheck_web/repositories/employee_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../models/document.dart';
+import '../../../repositories/picture_repository.dart';
 import '../../../services/local_storage_service.dart';
 import '../../../utils/simple_logger.dart';
 
@@ -13,25 +17,39 @@ class DetailsCubit extends Cubit<DetailsState> {
   final EmployeeRepository employeeRepository;
   final DocumentRepository documentsRepository; // Added DocumentsRepository
   final LocalStorageService localStorageService;
+  final FirebaseStorage storage;
 
   DetailsCubit(this.employeeRepository, this.documentsRepository,
-      this.localStorageService)
+      this.localStorageService, this.storage)
       : super(DetailsInitial());
 
   Future<void> getEmployeeAndDocuments(String employeeId) async {
     try {
       emit(DetailsLoading());
       Employee employee = await employeeRepository.getEmployeeById(employeeId);
-      List<Document> documents = await documentsRepository.getDocumentByEmployeeId(employeeId);
+      List<Document> documents =
+          await documentsRepository.getDocumentByEmployeeId(employeeId);
 
-      emit(DetailsLoaded(employee, documents));
+      List<String> urls = [];
+      // Fetch the documents from Firebase Storage
+      if (documents.isEmpty) {
+        emit(DetailsLoaded(employee, documents));
+        return;
+      }
+      for (var document in documents) {
+        try {
+          final url = await storage.ref(document.path).getDownloadURL();
+          urls.add(url);
+        } catch (e) {
+          emit(DetailsError('Failed to fetch user or documents: $e'));
+          SimpleLogger.warning('Failed to fetch document: $e');
+        }
+      }
+
+      emit(DetailsLoaded(employee, documents, urls: urls));
     } catch (e) {
       SimpleLogger.warning('Error fetching user or documents: $e');
       emit(DetailsError('Failed to fetch user or documents: $e'));
     }
   }
-
-//downloadDocument function where it retrieves the document from Firebase using the document id
-
-  
 }
